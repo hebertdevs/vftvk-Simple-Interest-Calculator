602801d7177cd97a3884504916f4dc134c7c0779
hebert <hebert.devs@gmail.com>
2022-03-08 22:41:46 -0300

    Project completion

--- script.js
+++ script.js
@@ -9,17 +9,10 @@ function compute(){
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
-    
-
     var interest = (principal * years * rate) /100;
     var year = new Date().getFullYear()+parseInt(years);
 
-    
-
     document.getElementById("result").innerHTML="<br\>If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";  
-
-    principal.style.backgroundColor='#900';
-    
 
     var caixa = document.getElementById("principal");  
 

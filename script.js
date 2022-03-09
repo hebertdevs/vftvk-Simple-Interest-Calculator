2ccfc68a9a985a4b7dff0931d6330c759baa4023
hebert <hebert.devs@gmail.com>
2022-03-08 22:31:51 -0300

    completion of the IBM course final project

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+};
+
+function compute(){  
+    var principal = document.getElementById("principal").value; 
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    
+
+    var interest = (principal * years * rate) /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    
+
+    document.getElementById("result").innerHTML="<br\>If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";  
+
+    principal.style.backgroundColor='#900';
+    
+
+    var caixa = document.getElementById("principal");  
+
+    if(principal <= 0 ){
+        alert("Enter a positive number"); 
+        caixa.focus();         
+        return  
+
+    }
     
 }
-        
+
+
